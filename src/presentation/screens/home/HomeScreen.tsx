8ca2e443de0a709b7a426658ca23210936d23677
iSlooZ <michel.dev26@gmail.com>
2024-04-04 18:21:18 -0300

    ToggleDrawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,25 @@
 import { Pressable, Text, View } from 'react-native'
 import { globalStyles } from '../../theme/theme';
-import { type NavigationProp, useNavigation } from '@react-navigation/native';
+import { type NavigationProp, useNavigation, DrawerActions } from '@react-navigation/native';
 import { PrimaryButton } from '../../components/shared/PrimaryButton';
 import { type RootStackParams } from '../../routes/StackNavigator';
+import { useEffect } from 'react';
 
 
 export const HomeScreen = () => {
 
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable onPress={ () => navigation.dispatch( DrawerActions.toggleDrawer )}>
+          <Text>Menu</Text>
+        </Pressable>
+      )
+    })
+   
+  }, [])
 
   return (
     <View style={globalStyles.container}>
